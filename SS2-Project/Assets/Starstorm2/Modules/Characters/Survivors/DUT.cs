408b24afa90a1fe583862fd52a2e1387aa0a40ab
swuff-star <63352264+swuff-star@users.noreply.github.com>
2024-07-11 22:17:20 -0400

    epic style
    started dut. controller and primary skill but untested. probably cant support modded skills ever, and unsure how to handle alts down the line :)

---
+++ SS2-Project/Assets/Starstorm2/Modules/Characters/Survivors/DUT.cs
@@ -0,0 +1,36 @@
+﻿using MSU;
+using R2API;
+using RoR2;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using static R2API.DamageAPI;
+using RoR2.ContentManagement;
+
+#if DEBUG
+namespace SS2.Survivors
+{
+
+    public sealed class DUT : SS2Survivor
+    {
+        public override SS2AssetRequest<SurvivorAssetCollection> AssetRequest => SS2Assets.LoadAssetAsync<SurvivorAssetCollection>("acDUT", SS2Bundle.Indev);
+
+        public override void Initialize()
+        {
+            ModifyPrefab();
+        }
+
+        public override bool IsAvailable(ContentPack contentPack)
+        {
+            return true;
+        }
+
+        public void ModifyPrefab()
+        {
+            var cb = CharacterPrefab.GetComponent<CharacterBody>();
+            cb.preferredPodPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Toolbot/RoboCratePod.prefab").WaitForCompletion(); ;
+            cb._defaultCrosshairPrefab = Resources.Load<GameObject>("Prefabs/Crosshair/StandardCrosshair");
+        }
+    }
+}
+#endif
